4de5ac1c1dafedd5bb6922425c4c1f57f1056269
unknown <joe@thekary.com>
2023-02-19 11:59:59 +0900

    yujong

---
+++ src/main/java/com/example/udemyblog/UdemyBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.udemyblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdemyBlogApplication.class, args);
+    }
+
+}
